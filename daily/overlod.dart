6ca3161060c691b969ea8bfd50f6572a0453e7cb
dhrupal-khunt <113020325+dhrupal-khunt@users.noreply.github.com>
2022-09-27 10:51:05 +0530

    s

---
+++ daily/overlod.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+class A {
+  void dispaly() {
+    print("hello");
+  }
+
+  int dispaly(int num) {
+    return 10;
+  }
+}
+
+void mai() {
+  var obj = A();
+  obj.dispaly();
+}
