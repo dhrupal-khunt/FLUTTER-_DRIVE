56ca2915a6f6531d03dfab8044ca094d360b7670
dhrupal-khunt <113020325+dhrupal-khunt@users.noreply.github.com>
2022-10-13 15:51:52 +0530

    done

--- ASSIGMENT/18.dart
+++ ASSIGMENT/18.dart
@@ -1,45 +1,45 @@
-// (18). write progarm of addition,subtraction,multiplication and division using switch case  (must be menu driven)//
-import 'dart:io';
+// // (18). write progarm of addition,subtraction,multiplication and division using switch case  (must be menu driven)//
+// import 'dart:io';
 
-class week {
-  late double num1, num2, add, sub, mul, divi;
-  late int sum;
+// class week {
+//   late double num1, num2, add, sub, mul, divi;
+//   late int sum;
 
-  void impot() {
-    print("enter number num1");
-    num1 = double.parse(stdin.readLineSync()!);
-    print("enter number num2");
-    num2 = double.parse(stdin.readLineSync()!);
-    print(
-        "1. addtion \n 2.subtraction \n 3.multiplication \n 4.division \n enter a number");
-    print("Enter your Choice : ");,
+//   void impot() {
+//     print("enter number num1");
+//     num1 = double.parse(stdin.readLineSync()!);
+//     print("enter number num2");
+//     num2 = double.parse(stdin.readLineSync()!);
+//     print(
+//         "1. addtion \n 2.subtraction \n 3.multiplication \n 4.division \n enter a number");
+//     print("Enter your Choice : ");,
 
-    switch (sum) {
-      case 1:
-        add = num1 + num2;
-        print(" addition is :$add");
-        break;
-      case 2:
-        sub = num1 - num2;
-        print("subraction is : $sub");
-        break;
-      case 3:
-        mul = num1 * num2;
-        print("multipication is : $mul");
-        break;
-      case 4:
-        divi = num1 / num2;
-        print("divition is : $divi");
-        break;
+//     switch (sum) {
+//       case 1:
+//         add = num1 + num2;
+//         print(" addition is :$add");
+//         break;
+//       case 2:
+//         sub = num1 - num2;
+//         print("subraction is : $sub");
+//         break;
+//       case 3:
+//         mul = num1 * num2;
+//         print("multipication is : $mul");
+//         break;
+//       case 4:
+//         divi = num1 / num2;
+//         print("divition is : $divi");
+//         break;
 
-      default:
-        print("Enter Your Correct Choice");
-        break;
-    }
-  }
-}
+//       default:
+//         print("Enter Your Correct Choice");
+//         break;
+//     }
+//   }
+// }
 
-void main() {
-  var obj = new week();
-  obj.impot();
-}
+// void main() {
+//   var obj = new week();
+//   obj.impot();
+// }
