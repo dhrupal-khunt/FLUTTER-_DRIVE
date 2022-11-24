9d6a559eb60352c436bcaec54d67b76757d6ac14
dhrupal-khunt <113020325+dhrupal-khunt@users.noreply.github.com>
2022-11-23 22:27:28 -0800

    6

--- flutter/mypro1/lib/main.dart
+++ flutter/mypro1/lib/main.dart
@@ -7,6 +7,7 @@ import 'package:mypro1/onescreen_twoscree/onescreen.dart';
 
 import 'package:mypro1/raitingbar/raitingar.dart';
 import 'package:mypro1/swich_on_off/swich_ex.dart';
+import 'package:mypro1/task/budget_loginpage.dart';
 import 'package:mypro1/traveling/7wonders.dart';
 import 'package:mypro1/traveling/hotel.dart';
 import 'package:mypro1/traveling/login.dart';
@@ -15,5 +16,5 @@ import 'package:mypro1/traveling/tab.dart';
 
 void main() {
   runApp(
-      MaterialApp(debugShowCheckedModeBanner: false, home: (login())));
+      MaterialApp(debugShowCheckedModeBanner: false, home: TrackerPage()));
 }
---
+++ flutter/mypro1/lib/task/budget_loginpage.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
+
+class TrackerPage extends StatefulWidget {
+  const TrackerPage({super.key});
+
+  @override
+  State<TrackerPage> createState() => _TrackerPageState();
+}
+
+class _TrackerPageState extends State<TrackerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(color: Color.fromARGB(255, 223, 204, 223)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ImageSlideshow(
+              indicatorColor: Colors.blue,
+              onPageChanged: (value) {
+                debugPrint('Page changed: $value');
+              },
+              autoPlayInterval: 3000,
+              isLoop: true,
+              children: [
+                Image.asset(
+                  "assets/images/2d.png",
+                  fit: BoxFit.fill,
+                ),
+                Image.asset(
+                  "assets/images/3dimage.png",
+                  fit: BoxFit.cover,
+                ),
+                Image.asset(
+                  "assets/images/4d.png",
+                  fit: BoxFit.cover,
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Container(
+                width: double.maxFinite,
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
+                  color: Color.fromARGB(255, 245, 241, 241),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      "bihguhfjpovvtewrverver",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      " e;kkjh bvwetcmkp y nih\nihichiigcseuthi kwe\nhitwtchxtt",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    SizedBox(
+                        width: double.maxFinite,
+                        height: 48,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text("Let's Start"),
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.deepPurple,
+                          ),
+                        ))
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
